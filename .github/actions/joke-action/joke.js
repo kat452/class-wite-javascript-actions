d08a91266001c4c3291148e920181500ecef49da
kat452 <53538637+kat452@users.noreply.github.com>
2025-04-15 14:38:13 -0500

    Update joke.js

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -1,22 +1,18 @@
-/*const request = require("request-promise-core");
-
-*/
+const request = require("request-promise");
 
 const options = {
-    method: "GET",
-    uri: "https://icanhazdadjoke.com/",
-    headers: {
-        Accept: "application/json",
-        "User-Agent": "Writing JavaScript action GitHub Skills course.",
-    },
-    json: true,
+  method: "GET",
+  uri: "https://icanhazdadjoke.com/",
+  headers: {
+    Accept: "application/json",
+    "User-Agent": "Writing JavaScript action GitHub Skills course.",
+  },
+  json: true,
 };
+
 async function getJoke() {
-    const res = fetch("https://icanhazdadjoke.com/", options)
-        .then((res) => res.json())
-        .then((data) => console.log(data.joke))
-        .catch((err) => console.error(err));
-    return res.joke;
+  const res = await request(options);
+  return res.joke;
 }
 
 module.exports = getJoke;
